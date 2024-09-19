5991a0ea49dd7aae58b8f89f9f0a951f1dc1c70a
nepikn <31944706+nepikn@users.noreply.github.com>
2024-09-19 20:00:23 +0800

    feat(dashboard): view post actions

--- packages/client/src/routes/dashboard/children/category.jsx
+++ packages/client/src/routes/dashboard/children/category.jsx
@@ -1,10 +1,20 @@
-import { Image } from "@mui/icons-material";
+import {
+  BookmarkBorderOutlined,
+  ChatBubbleOutlineOutlined,
+  Image,
+  MoreVert,
+  SentimentSatisfiedOutlined,
+  ThumbUpOutlined,
+} from "@mui/icons-material";
 import {
   Avatar,
+  Button,
   Card,
+  CardActions,
   CardContent,
   CardHeader,
   CardMedia,
+  IconButton,
   List,
   ListItem,
   Stack,
@@ -28,19 +38,52 @@ export default function Category({ children }) {
 
 function Post({ post }) {
   const { author, title, abstract } = post;
+  const btns = [
+    {
+      StartIcon: SentimentSatisfiedOutlined,
+      children: "380",
+    },
+    {
+      StartIcon: ThumbUpOutlined,
+      children: "121",
+    },
+    {
+      StartIcon: ChatBubbleOutlineOutlined,
+      children: "12",
+      disabled: true,
+    },
+  ];
 
   return (
     <Card component={"article"}>
-      <CardHeader avatar={<Avatar />} title={author} />
+      <CardHeader
+        avatar={<Avatar />}
+        title={author}
+        action={
+          <IconButton disabled>
+            <MoreVert />
+          </IconButton>
+        }
+      />
       <Stack direction={"row"} sx={{ alignItems: "center" }}>
-        <CardContent>
-          <Stack spacing={2}>
-            <Typography variant="h2">{title}</Typography>
-            <Typography variant="body1" color="textSecondary">
-              {abstract}
-            </Typography>
-          </Stack>
-        </CardContent>
+        <Stack>
+          <CardContent component={Typography} variant="h2">
+            {title}
+          </CardContent>
+          <CardContent
+            component={Typography}
+            variant="body1"
+            color="textSecondary"
+          >
+            {abstract}
+          </CardContent>
+          <CardActions disableSpacing>
+            <Actions btns={btns} />
+            <IconButton sx={{ ml: "auto" }}>
+              <BookmarkBorderOutlined />
+            </IconButton>
+          </CardActions>
+        </Stack>
         <CardMedia sx={{ width: 1 / 3, flexShrink: 0 }}>
           <Image
             sx={{
@@ -52,5 +95,27 @@ function Post({ post }) {
         </CardMedia>
       </Stack>
     </Card>
+  );
+}
+
+function Actions({ btns }) {
+  return (
+    <>
+      {btns.map(({ StartIcon, children, ...props }, i) => {
+        const value = StartIcon.name;
+
+        return (
+          <Button
+            key={value}
+            name="intent"
+            value={value}
+            startIcon={<StartIcon />}
+            {...props}
+          >
+            {children}
+          </Button>
+        );
+      })}
+    </>
   );
 }
