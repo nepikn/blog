39fd2e72eaa91508f8f2c9600527cab9d1d72ab8
nepikn <31944706+nepikn@users.noreply.github.com>
2024-09-19 09:51:05 +0800

    refactor(posts): implent <Card>

--- packages/client/src/routes/dashboard/following.jsx
+++ packages/client/src/routes/dashboard/following.jsx
@@ -1,57 +1,56 @@
 import { Image } from "@mui/icons-material";
 import {
   Avatar,
-  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
   List,
   ListItem,
-  ListItemAvatar,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
   Stack,
   Typography,
 } from "@mui/material";
 import { useLoaderData } from "react-router-dom";
 
-export default function Following({ children }) {
+export default function Category({ children }) {
   const { data: posts } = useLoaderData();
 
   return (
     <List>
-      {posts.map(({ author, title, abstract }, i) => (
-        <ListItem divider key={title}>
-          <List>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar />
-              </ListItemAvatar>
-              <ListItemText primary={author} />
-            </ListItem>
-            <ListItem>
-              <ListItemText>
-                <Stack spacing={2}>
-                  <Typography variant="h2">{title}</Typography>
-                  <Typography
-                    variant="body1"
-                    color="textSecondary"
-                  >
-                    {abstract}
-                  </Typography>
-                </Stack>
-              </ListItemText>
-              <ListItemIcon sx={{ width: 1 / 4, flexShrink: 0 }}>
-                <Image
-                  sx={{
-                    aspectRatio: 1,
-                    width: 1,
-                    height: "auto",
-                  }}
-                />
-              </ListItemIcon>
-            </ListItem>
-          </List>
+      {posts.map((post, i) => (
+        <ListItem key={post.title}>
+          <Post post={post} />
         </ListItem>
       ))}
     </List>
+  );
+}
+
+function Post({ post }) {
+  const { author, title, abstract } = post;
+
+  return (
+    <Card component={"article"}>
+      <CardHeader avatar={<Avatar />} title={author} />
+      <Stack direction={"row"} sx={{ alignItems: "center" }}>
+        <CardContent>
+          <Stack spacing={2}>
+            <Typography variant="h2">{title}</Typography>
+            <Typography variant="body1" color="textSecondary">
+              {abstract}
+            </Typography>
+          </Stack>
+        </CardContent>
+        <CardMedia sx={{ width: 1 / 3, flexShrink: 0 }}>
+          <Image
+            sx={{
+              aspectRatio: 1,
+              width: 1,
+              height: "auto",
+            }}
+          />
+        </CardMedia>
+      </Stack>
+    </Card>
   );
 }
