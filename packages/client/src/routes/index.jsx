7539ee4bf9b324898d3430b12e90df280c405b4a
nepikn <31944706+nepikn@users.noreply.github.com>
2024-09-27 13:37:19 +0800

    refactor: getIndexedChildren

--- packages/client/src/routes/index.jsx
+++ packages/client/src/routes/index.jsx
@@ -5,58 +5,64 @@ import Error from "./error";
 import Private from "./private";
 import Layout from "./root/layout";
 
+/** @type {import("react-router-dom").RouteObject} */
 export default {
   errorElement: <Error root />,
   element: <Layout />,
   loader: api.auth,
   action: api.auth,
-  children: [
+  children: getIndexedChildren(
+    {
+      lazy: () => import("./root"),
+      action: api.auth,
+    },
+    {
+      path: "dashboard",
+      lazy: async () => {
+        const { Component } = await import("./dashboard");
+        return {
+          element: (
+            <Private>
+              <Component />
+            </Private>
+          ),
+        };
+      },
+      children: getIndexedChildren(
+        {
+          loader: () => redirect(childRoutes[0].path),
+        },
+        {
+          path: ":category",
+          lazy: async () => {
+            const { Category } = await import(
+              "../routes/dashboard"
+            );
+            return {
+              element: <Category />,
+            };
+          },
+          loader: api.post,
+          action: api.post,
+        },
+      ),
+    },
+  ),
+};
+
+/** @param {import("react-router-dom").IndexRouteObject} indexProps */
+/** @param {import("react-router-dom").NonIndexRouteObject[]} children */
+function getIndexedChildren(indexProps, ...children) {
+  return [
     {
       errorElement: <Error />,
       children: [
         {
           index: true,
-          lazy: () => import("./root"),
-          action: api.auth,
+          ...indexProps,
         },
-        {
-          path: "dashboard",
-          lazy: async () => {
-            const { Component } = await import("./dashboard");
-            return {
-              element: (
-                <Private>
-                  <Component />
-                </Private>
-              ),
-            };
-          },
-          children: [
-            {
-              errorElement: <Error />,
-              children: [
-                {
-                  index: true,
-                  loader: () => redirect(childRoutes[0].path),
-                },
-                {
-                  path: ":category",
-                  lazy: async () => {
-                    const { Category } = await import(
-                      "../routes/dashboard"
-                    );
-                    return {
-                      element: <Category />,
-                    };
-                  },
-                  loader: api.post,
-                  action: api.post,
-                },
-              ],
-            },
-          ],
-        },
+        ...children,
       ],
     },
-  ],
-};
+  ];
+}
