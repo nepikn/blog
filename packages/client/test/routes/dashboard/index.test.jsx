5163028960abd3fbd2cffc4f9b39e0c7d21e0b1b
nepikn <31944706+nepikn@users.noreply.github.com>
2024-10-24 02:51:22 +0800

    test: <Dashboard>

---
+++ packages/client/test/routes/dashboard/index.test.jsx
@@ -0,0 +1,49 @@
+import { screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import api from "../../../src/api";
+import Auth from "../../../src/contexts/auth";
+import {
+  Category,
+  Component,
+} from "../../../src/routes/dashboard";
+import { setup } from "../../setup";
+
+describe("Dashboard", () => {
+  vi.mock("localforage");
+
+  it("changes category when tabs clicked", async () => {
+    setup(
+      {
+        entry: "/following",
+        path: "/",
+        element: (
+          <Auth.Provider value={{ name: "owo" }}>
+            <Component />
+          </Auth.Provider>
+        ),
+        children: [
+          {
+            path: ":category",
+            element: <Category />,
+            loader: api.post,
+            action: api.post,
+          },
+        ],
+      },
+      async (user) => {
+        await screen.findByRole("heading", {
+          name: /exploring the future/i,
+        });
+        await user.click(
+          screen.getByRole("tab", { name: /trending/i }),
+        );
+      },
+    );
+
+    expect(
+      await screen.findByRole("heading", {
+        name: /machine learning/i,
+      }),
+    );
+  });
+});
