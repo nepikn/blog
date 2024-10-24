68c34fd3a8d0091c0d28af337c96179a32b0743a
nepikn <31944706+nepikn@users.noreply.github.com>
2024-10-25 03:32:06 +0800

    chore(server): clean unneeded

--- packages/server/start/db.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER IF NOT EXISTS 'admin' @'localhost' IDENTIFIED BY 'auth_string';
-
-CREATE DATABASE IF NOT EXISTS forum_sequelize;
-
-GRANT ALL ON forum_sequelize.* TO 'admin' @'localhost';
